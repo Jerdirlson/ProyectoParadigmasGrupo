2487a272ebbdcf17ae1b4dbd8b712549f2067575
Nooojuan05 <jsanabriac54@gmail.com>
2022-10-15 13:00:21 -0500

    Jego Maquina

--- ProyectoParadigmas/src/ramdomGenerate.java
+++ ProyectoParadigmas/src/ramdomGenerate.java
@@ -4,12 +4,20 @@ public class ramdomGenerate {
 	public ramdomGenerate() {
 		// TODO Auto-generated constructor stub
 	}
-	
 	//----------Genera un numero ramdom desde el 0 hasta el numro solicitado por el parametro
 	
+	Random numAleatorio = new Random();
+	
+	//----------Genera un numero ramdom desde el 0 hasta el numro solicitado por el parametro
 	public int generarNumTab(int rango) {
-		Random numAleatorio = new Random();
-		int n = numAleatorio.nextInt(rango-0+1);
-		return n;
+		int numero = numAleatorio.nextInt(rango-0+1);
+		return numero;
 	}
+
+	public int eleccionMaquina(int rango) {
+		int numero = numAleatorio.nextInt(rango-0+1);
+		return numero;
+	}
+
+
 }
