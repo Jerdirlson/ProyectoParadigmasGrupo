a9569d604d4b6c10258fb4b6046819d7d0e46362
Nooojuan05 <jsanabriac54@gmail.com>
2022-10-16 16:13:22 -0500

    Arreglos en el cronometro

--- ProyectoParadigmas/src/Tableros.java
+++ ProyectoParadigmas/src/Tableros.java
@@ -14,7 +14,6 @@ import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
 import javax.swing.JOptionPane;
 
-
 //Cuando se termine el juego se tiene que parar el contador 
 public class Tableros implements ActionListener{
 private JLabel fondoPanel, fondoPanelGeneral;
@@ -37,12 +36,13 @@ private JLabel fondoPanel, fondoPanelGeneral;
 	private int posX;	//Posicion x de la carta
 	private int posY;	//Posicion y de la carta
 
-	private int seg,min; //Para el contador de segundos
-	private Timer tiempo;
-	private JLabel cronometro;
+	static private int seg,min; //Para el contador de segundos
+	static private Timer tiempo;
+	static JLabel cronometro;
+	private TimerTask tarea;
 
-	private int puntuacion = 0; //Puntuacion acumulada
-	private int puntuacionJugador2 = 0;
+	int puntuacion = 0; //Puntuacion acumulada
+	int puntuacionJugador2 = 0;
 
 	private int CartasAjugar;
 	private int contadorPares = 0;
@@ -72,7 +72,6 @@ private JLabel fondoPanel, fondoPanelGeneral;
 	public void componentes() {
 
 			//-------------------------------------------Panel------------------------------------
-			
 			//Propiedades JPanel
 			panel = new JLayeredPane();
 			panel.setLayout(null);	//Esto nos permite colocar los demas componentes donde queramos
@@ -229,7 +228,14 @@ private JLabel fondoPanel, fondoPanelGeneral;
 	public int getContador() {
 		return contador;
 	}
-		
+
+	public int getMin(){
+		return min;
+	}
+	
+	public int getSeg(){
+		return seg;
+	}
 	//-----Metodo para regresar un boton al estado normal cuando las cartas son diferentes  --------
 	public void botonEstadoNormal() {
 			
@@ -367,11 +373,15 @@ private JLabel fondoPanel, fondoPanelGeneral;
 		juegoTerminado();
 	}
 	
-	public void Timer(){
+	public void cronometro(){
+		
+		
+		
+		
 			min = 0;
 			seg = 0;
 			tiempo = new Timer();
-			TimerTask tarea = new TimerTask(){
+			tarea = new TimerTask(){
 
 			public void run(){
 				seg++;
@@ -379,10 +389,17 @@ private JLabel fondoPanel, fondoPanelGeneral;
 					min++;
 					seg = 0;
 				}
-				cronometro.setText("Tiempo: " + min +":" +seg);
+				if(seg<10){
+					cronometro.setText("Tiempo " + min +":" +"0"+seg);
+
+				}
+				else{
+					cronometro.setText("Tiempo " + min +":"+seg);
+				}
+				
 			}
 			};
-		tiempo.schedule(tarea,0,1000);
+		tiempo.schedule(tarea,1,1000);
 	}
 	
 	//Actualiza la puntiación del jugador
@@ -553,7 +570,7 @@ private JLabel fondoPanel, fondoPanelGeneral;
 			//Metodos modalidad Individual
 			modalidadIndividual(size);
 			labelPuntuacionJugadorUno();
-			Timer();
+			cronometro();
 			break;
 		case 2:
 			
@@ -563,18 +580,18 @@ private JLabel fondoPanel, fondoPanelGeneral;
 			labelPuntuacionJugadorUno();
 			labelNombreJugadorDos();
 			labelPuntuacionJugadorDos();
-			Timer();
+			cronometro();
 			break;
 		case 3:
 			
 		ModalidadJugadorMaquina(size);
 		
 		labelPuntuacionJugadorUno();
-		
 		labelNombreJugadorDos();
 		labelNombreJugadorDos.setText("MAQUINA");
 		labelPuntuacionJugadorDos();
 		turnoJugador();
+		cronometro();
 		
 
 
@@ -1056,25 +1073,52 @@ private JLabel fondoPanel, fondoPanelGeneral;
 		};
 			timer.schedule(tarea, 3700);	
 	}
-		
+	
+	public JLabel nombreGanador(){
+
+		if(puntuacion > puntuacionJugador2){
+			return nombreJugador;
+		}
+		else{
+			return labelNombreJugadorDos;
+		}
+	}
+
+	public int puntuacionGanador(){
+
+		if(nombreGanador().getText() == nombreJugador.getText()){
+			return puntuacion;
+		}
+		else{
+			return puntuacionJugador2;
+		}
+	}
+
+
 	public void juegoTerminado() {	
 		if(contadorPares == CartasAjugar) {
+
 			System.out.println("El juego terminó");
-			
-		
+			tarea.cancel();
+			tiempo.cancel();
+			String mensaje = "El ganador fue: " + nombreGanador().getText() + " Su puntuacion fue: " + puntuacionGanador()+ " Con un tiempo total de " + getMin() + ":" + getSeg();
+			JOptionPane.showMessageDialog(null, mensaje, "Ganador!!", JOptionPane.INFORMATION_MESSAGE);
+			MenuDeInicio ventana = new MenuDeInicio();
+			ventana.setVisible(true);
+	      frame.setVisible(false);
 		}
 	}
 
 	public void actionPerformed(ActionEvent e) {
 	   
 	   if (e.getSource() == botonSalir) {
-	   
+		tarea.cancel();
+		tiempo.cancel();
          
 			MenuDeInicio ventana = new MenuDeInicio();
 			ventana.setVisible(true);
-	      
 	      frame.setVisible(false);
-	    }
+	   }
 	}
 
 	public Tableros() {	
